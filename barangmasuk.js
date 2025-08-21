ad964d26b5b1729d30c55c1d9fc55aeb5ab4f786
PriycellAfrizal <142721369+PriycellAfrizal@users.noreply.github.com>
2025-08-21 13:46:08 +0700

    Update barangmasuk.js

--- barangmasuk.js
+++ barangmasuk.js
@@ -162,6 +162,73 @@
               });
             }
 
+$(document).ready(function () {
+    $('#kodeproduksi').select2({
+        ajax: {
+            url: 'warehouse/get_kodeproduksi.php',
+            dataType: 'json',
+            delay: 250,
+            processResults: function (data) {
+                console.log(data); // Log the received data to the console
+                return {
+                    results: data
+                };
+            },
+            cache: true
+        },
+        language: {
+            searching: function () {
+                return 'Mencari...';
+            }
+        },
+        placeholder: 'Cari...',
+        minimumInputLength: 0,
+        allowClear: true,
+        formatNoMatches: function () {
+            return 'Tidak ditemukan hasil';
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+function carispk() {
+    // Get the selected nama barang
+    var selectedKodeProduksi = $("#kodeproduksi").val();
+
+    // Make an AJAX request to get the corresponding kodebarang
+    $.ajax({
+        type: "GET",
+        url: "warehouse/get_spk.php", // Replace with the actual server-side script to handle the data
+        data: { kodeproduksi: selectedKodeProduksi },
+        dataType: 'json',
+        success: function(response) {
+            // Update the hidden input field with the fetched kodebarang
+
+            $("#spk").val(response.spk);
+
+          
+        },
+        error: function(error) {
+            // Handle errors, if any
+            console.error(error);
+        }
+    });
+}
+
+
+
+
+
+
+
+
 
 
             $(document).ready(function() {
