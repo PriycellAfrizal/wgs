87443b31454b43615e7a111282c0e180ae0e33e3
PriycellAfrizal <142721369+PriycellAfrizal@users.noreply.github.com>
2025-09-09 09:42:56 +0700

    Update masterchassis.js

--- masterchassis.js
+++ masterchassis.js
@@ -1,10 +1,3 @@
-$(document).ready(function () {
-    // Inisialisasi DataTables
-    $('#myTable').DataTable();       // ID From dataTable 
-    $('#dataTable').DataTable();     // ID From dataTable 
-    $('#dataTableHover').DataTable(); // ID From dataTable with Hover
-});
-
 function simpanmasterchasis() {
     // Mendapatkan nilai dari input dengan id "type" dan "skrb"
     var typeValue = document.getElementById("type").value;
