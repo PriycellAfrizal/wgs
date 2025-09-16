9f6169c308eb2f2314efd326ca75260fa91f8a79
PriycellAfrizal <142721369+PriycellAfrizal@users.noreply.github.com>
2025-09-16 10:32:22 +0700

    Update editmaster.js

--- editmaster.js
+++ editmaster.js
@@ -116,9 +116,7 @@ function saveChanges() {
         maxstock: $("#maxstockEdit").val(),
         tipe: $("#tipeEdit").val(),
         classValue: $("#classEdit").val(),
-        sn: $("#snEdit").val(),
-        
-    namaedit: $("#namaedit").val()  // <--- tambah ini
+        sn: $("#snEdit").val()
     };
 
     // Validasi
